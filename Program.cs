a9a27d5a99e708d11a57740fdcee3407ad30ea1f
Jonathan Bodrero <jbodrero@hotmail.com>
2025-05-27 10:35:14 -0600

    Two space Mario

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿
-//  Lab 1.5  Draw Mario
-using System.Drawing;
+﻿//Mario2  (2 spaces per character)
+
+//  Lab 1.5   Draw Mario
+//using System.Drawing;
 
 // Clear the screen
 //Console.BackgroundColor = ConsoleColor.Black;
@@ -9,136 +10,137 @@ Console.Clear();
 Console.SetCursorPosition(0, 0);
 
 // Draw cyan background
-Console.BackgroundColor = ConsoleColor.Cyan;
-Console.WriteLine("AAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA \nAAAAAAAAAAAAAA");
+Console.BackgroundColor = ConsoleColor. Cyan;
+Console.Write("                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                             \n                            ");
 
-//Draw red squares
+// Draw red squares
 Console.BackgroundColor = ConsoleColor.Red;
-Console.SetCursorPosition(4,1);
-Console.WriteLine("BBBBB");
-Console.SetCursorPosition(3,2);
-Console.WriteLine("BBBBBBBBB");
-Console.SetCursorPosition(5,8);
-Console.WriteLine("B");
-Console.SetCursorPosition(5,9);
-Console.WriteLine("B");
-Console.SetCursorPosition(8,9);
-Console.WriteLine("B");
-Console.SetCursorPosition(5,10);
-Console.WriteLine("B");
-Console.SetCursorPosition(8,10);
-Console.WriteLine("B");
-Console.SetCursorPosition(5,11);
-Console.WriteLine("BBBB");
-Console.SetCursorPosition(4,12);
-Console.WriteLine("BBBBBB");
-Console.SetCursorPosition(3,13);
-Console.WriteLine("BBBBBBBB");
-Console.SetCursorPosition(3,14);
-Console.WriteLine("BBB");
-Console.SetCursorPosition(8,14);
-Console.WriteLine("BBB");
-
-//Draw black squares
-Console.BackgroundColor = ConsoleColor.Black;
-Console.SetCursorPosition(3,3);
-Console.WriteLine("CCC");
-Console.SetCursorPosition(8,3);
-Console.WriteLine("C");
-Console.SetCursorPosition(2,4);
-Console.WriteLine("C");
-Console.SetCursorPosition(4,4);
-Console.WriteLine("C");
-Console.SetCursorPosition(8,4);
-Console.WriteLine("C");
-Console.SetCursorPosition(2,5);
-Console.WriteLine("C");
-Console.SetCursorPosition(4,5);
-Console.WriteLine("CC");
-Console.SetCursorPosition(9,5);
-Console.WriteLine("C");
-Console.SetCursorPosition(2,6);
-Console.WriteLine("CC");
-Console.SetCursorPosition(8,6);
-Console.WriteLine("CCCC");
-
-//Draw tan (white) squares
-Console.BackgroundColor = ConsoleColor.White;
-Console.SetCursorPosition(6,3);
-Console.WriteLine("DD");
-Console.SetCursorPosition(9,3);
-Console.WriteLine("D");
-Console.SetCursorPosition(3, 4);
-Console.WriteLine("D");
-Console.SetCursorPosition(5, 4);
-Console.WriteLine("DDD");
-Console.SetCursorPosition(9, 4);
-Console.WriteLine("DDD");
-Console.SetCursorPosition(3, 5);
-Console.WriteLine("D");
-Console.SetCursorPosition(6, 5);
-Console.WriteLine("DDD");
-Console.SetCursorPosition(10, 5);
-Console.WriteLine("DDD");
-Console.SetCursorPosition(4, 6);
-Console.WriteLine("DDDD");
-Console.SetCursorPosition(4, 7);
-Console.WriteLine("DDDDDDD");
-Console.SetCursorPosition(1, 11);
-Console.WriteLine("DD");
-Console.SetCursorPosition(11, 11);
-Console.WriteLine("DD");
-Console.SetCursorPosition(1, 12);
-Console.WriteLine("DDD");
-Console.SetCursorPosition(10, 12);
-Console.WriteLine("DDD");
-Console.SetCursorPosition(1, 13);
-Console.WriteLine("DD");
-Console.SetCursorPosition(11, 13);
-Console.WriteLine("DD");
-Console.SetCursorPosition(1, 17);
-
-//Draw dark blue squares
-Console.BackgroundColor = ConsoleColor.DarkBlue;
-Console.SetCursorPosition(3,8);
-Console.WriteLine("EE");
-Console.SetCursorPosition(6,8);
-Console.WriteLine("EEE");
-Console.SetCursorPosition(2,9);
-Console.WriteLine("EEE");
-Console.SetCursorPosition(6,9);
-Console.WriteLine("EE");
-Console.SetCursorPosition(9,9);
-Console.WriteLine("EEE");
-Console.SetCursorPosition(1,10);
-Console.WriteLine("EEEE");
-Console.SetCursorPosition(6,10);
-Console.WriteLine("EE");
-Console.SetCursorPosition(9,10);
-Console.WriteLine("EEEE");
-Console.SetCursorPosition(3,11);
-Console.WriteLine("EE");
-Console.SetCursorPosition(9,11);
-Console.WriteLine("EE");
-
-// Draw yellow squares
-Console.BackgroundColor = ConsoleColor.DarkYellow;
-Console.SetCursorPosition(5,12);
-Console.WriteLine("F");
+Console.SetCursorPosition(8,1);
+Console.Write("          ");
+Console.SetCursorPosition(6,2);
+Console.Write("                  ");
+Console.SetCursorPosition(10,8);
+Console.Write("  ");
+Console.SetCursorPosition(10,9);
+Console.Write("  ");
+Console.SetCursorPosition(16,9);
+Console.Write("  ");
+Console.SetCursorPosition(10,10);
+Console.Write("  ");
+Console.SetCursorPosition(16,10);
+Console.Write("  ");
+Console.SetCursorPosition(10,11);
+Console.Write("        ");
 Console.SetCursorPosition(8,12);
-Console.WriteLine("F");
+Console.Write("            ");
+Console.SetCursorPosition(6,13);
+Console.Write("                ");
+Console.SetCursorPosition(6,14);
+Console.Write("      ");
+Console.SetCursorPosition(16,14);
+Console.Write("      ");
 
-// Draw brown squares
+// Draw black squares
+Console.BackgroundColor = ConsoleColor.Black;
+Console.SetCursorPosition(6,3);
+Console.Write("      ");
+Console.SetCursorPosition(16,3);
+Console.Write("  ");
+Console.SetCursorPosition(4,4);
+Console.Write("  ");
+Console.SetCursorPosition(8,4);
+Console.Write("  ");
+Console.SetCursorPosition(16,4);
+Console.Write("  ");
+Console.SetCursorPosition(4,5);
+Console.Write("  ");
+Console.SetCursorPosition(8,5);
+Console.Write("    ");
+Console.SetCursorPosition(18,5);
+Console.Write("  ");
+Console.SetCursorPosition(8,6);
+Console.Write("    ");
+Console.SetCursorPosition(16,6);
+Console.Write("        ");
+
+// Draw tan (white) squares
+Console.BackgroundColor = ConsoleColor.White;
+Console.SetCursorPosition(12,3);
+Console.Write("    ");
+Console.SetCursorPosition(18,3);
+Console.Write("  ");
+Console.SetCursorPosition(6, 4);
+Console.Write("  ");
+Console.SetCursorPosition(10, 4);
+Console.Write("      ");
+Console.SetCursorPosition(18, 4);
+Console.Write("      ");
+Console.SetCursorPosition(6, 5);
+Console.Write("  ");
+Console.SetCursorPosition(12, 5);
+Console.Write("      ");
+Console.SetCursorPosition(20, 5);
+Console.Write("      ");
+Console.SetCursorPosition(8, 6);
+Console.Write("        ");
+Console.SetCursorPosition(8, 7);
+Console.Write("              ");
+Console.SetCursorPosition(2, 11);
+Console.Write("    ");
+Console.SetCursorPosition(22, 11);
+Console.Write("    ");
+Console.SetCursorPosition(2, 12);
+Console.Write("      ");
+Console.SetCursorPosition(20, 12);
+Console.Write("      ");
+Console.SetCursorPosition(2, 13);
+Console.Write("    ");
+Console.SetCursorPosition(22, 13);
+Console.Write("    ");
+Console.SetCursorPosition(2, 17);
+
+// Draw dark blue squares
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.SetCursorPosition(6,8);
+Console.Write("    ");
+Console.SetCursorPosition(12,8);
+Console.Write("      ");
+Console.SetCursorPosition(4,9);
+Console.Write("      ");
+Console.SetCursorPosition(12,9);
+Console.Write("    ");
+Console.SetCursorPosition(18,9);
+Console.Write("      ");
+Console.SetCursorPosition(2,10);
+Console.Write("        ");
+Console.SetCursorPosition(12,10);
+Console.Write("    ");
+Console.SetCursorPosition(18,10);
+Console.Write("        ");
+Console.SetCursorPosition(6,11);
+Console.Write("    ");
+Console.SetCursorPosition(18,11);
+Console.Write("    ");
+
+//  Draw yellow squares
+Console.BackgroundColor = ConsoleColor. DarkYellow;
+Console.SetCursorPosition(10,12);
+Console.Write("  ");
+Console.SetCursorPosition(16,12);
+Console.Write("  ");
+
+//  Draw brown squares
 Console.BackgroundColor = ConsoleColor.DarkGray;
-Console.SetCursorPosition(2,15);
-Console.WriteLine("GGG");
-Console.SetCursorPosition(8,15);
-Console.WriteLine("GGG");
-Console.SetCursorPosition(1,16);
-Console.WriteLine("GGGG");
-Console.SetCursorPosition(8,16);
-Console.WriteLine("GGGG");
+Console.SetCursorPosition(4,15);
+Console.Write("      ");
+Console.SetCursorPosition(16,15);
+Console.Write("      ");
+Console.SetCursorPosition(2,16);
+Console.Write("        ");
+Console.SetCursorPosition(16,16);
+Console.Write("        ");
 
 Console.BackgroundColor = ConsoleColor.Black;
-Console.SetCursorPosition(18,18);
-//Console.WriteLine("xyz");
+Console.SetCursorPosition(36,18);
+//Console.Write("xyz");
+
